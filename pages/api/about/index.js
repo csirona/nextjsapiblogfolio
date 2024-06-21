faf5aa18627caeeb8b3c803c0225ece648e2d6a3
csirona <108961876+csirona@users.noreply.github.com>
2024-06-21 13:23:17 -0400

    Update index.js

--- pages/api/about/index.js
+++ pages/api/about/index.js
@@ -4,7 +4,13 @@ import path from 'path';
 const filePath = path.join(process.cwd(), 'data', 'about.json');
 
 export default function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
   if (req.method === 'GET') {
     try {
       const fileContents = fs.readFileSync(filePath, 'utf8');
