d7eb54b8a32cd676479e0be581ee8db3e6a14717
csirona <108961876+csirona@users.noreply.github.com>
2024-06-21 12:50:36 -0400

    Update index.js

--- pages/api/projects/index.js
+++ pages/api/projects/index.js
@@ -3,9 +3,13 @@ import path from 'path';
 
 // Middleware to handle CORS
 const allowCors = (fn) => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+ res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
   // Handle preflight request
   if (req.method === 'OPTIONS') {
     res.status(200).end();
